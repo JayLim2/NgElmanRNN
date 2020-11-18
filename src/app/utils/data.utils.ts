a019dfb799c57519034a06fedde6a5b90e73302e
JayLim <ddx180874@gmail.com>
2020-11-18 13:51:06 +0400

    some functions and configs

--- src/app/utils/data.utils.ts
+++ src/app/utils/data.utils.ts
@@ -11,6 +11,7 @@ export class DataUtils {
   private _testingSamples: number[];
 
   private configuration = {
+    epochs: 1000,
     training: 70,
     validation: 15,
     testing: 15,
@@ -19,6 +20,9 @@ export class DataUtils {
     hiddenCount: 0,
     outputCount: 0
   };
+
+  private _inputWeights: Matrix = null;
+  private _hiddenWeights: Matrix = null;
 
   constructor(
     private http: HttpClient
@@ -40,87 +44,6 @@ export class DataUtils {
   get testingSamples(): number[] {
     return this._testingSamples;
   }
-
-  public configure(training: number = 70, validation: number = 15) {
-    if (training + validation > 100) {
-      throw new Error("Training, testing and validation percents must summary equals 100.");
-    }
-    this.configuration.training = training;
-    this.configuration.validation = validation;
-    this.configuration.testing = 100 - training - validation;
-  }
-
-  public distribute(samples: number[]) {
-    if (samples && samples.length > 0) {
-      let length = samples.length;
-      let trainingLength = this.getNewLength(length, this.configuration.training);
-      let validationLength = this.getNewLength(length, this.configuration.validation);
-      this._trainingSamples = samples.slice(0, trainingLength);
-      this._validationSamples = samples.slice(trainingLength, trainingLength + validationLength);
-      this._testingSamples = samples.slice(trainingLength + validationLength, length);
-
-      console.log(this._trainingSamples);
-      console.log(this._validationSamples);
-      console.log(this._testingSamples);
-    }
-  }
-
-  public getNewLength(length: number = 0, percent: number = 0) {
-    if (length !== 0 && percent !== 0) {
-      return Math.floor((percent / 100) * length);
-    }
-  }
-
-  public datasetToVector() {
-    this.getDataset().subscribe((dataset) => {
-      this._currentDataset = dataset;
-
-      let lines = dataset.split("\n")
-        .map(line => line.trim())
-        .filter(line => line !== "");
-      let classes = new Set();
-      let resultVector = [];
-      for (const lineStr of lines) {
-        let line = lineStr.split(" ").map(value => value.trim());
-        line = line.slice(1, line.length);
-        let clazz = line[line.length - 1];
-        classes.add(clazz);
-        let lineVector = [
-          ...line.slice(0, line.length - 1).map(element => Number.parseFloat(element)),
-          this.indexOf(clazz, classes)
-        ];
-        resultVector.push(lineVector);
-      }
-      this.configuration.outputCount = 1;
-      this.configuration.inputCount = resultVector[0].length - this.configuration.outputCount;
-      this.configuration.hiddenCount = Math.round((this.configuration.inputCount + this.configuration.outputCount) / 2);
-      this.configuration.contextCount = this.configuration.hiddenCount;
-      this.distribute(resultVector);
-
-    }, (error) => {
-      console.error(error);
-    })
-  }
-
-  private indexOf(value: any, set: Set<any>): number {
-    let i = 0;
-    for (const element of set) {
-      if (element === value) {
-        return i;
-      }
-      i++;
-    }
-    return -1;
-  }
-
-  private getDataset() {
-    return this.http.get("../../assets/irisDataset.txt", {
-      responseType: "text"
-    });
-  }
-
-  private _inputWeights: Matrix = null;
-  private _hiddenWeights: Matrix = null;
 
   get inputWeights(): Matrix {
     if (!this._inputWeights) {
@@ -152,6 +75,110 @@ export class DataUtils {
       this._hiddenWeights = matrix;
     }
     return this._hiddenWeights;
+  }
+
+  public configure(epochs: number = 1000, training: number = 70, validation: number = 15) {
+    if (epochs <= 0) {
+      throw new Error("Epochs count must be greater than 0.");
+    }
+    if (training <= 0 || validation <= 0) {
+      throw new Error("Samples size must be greater than 0.");
+    }
+    if (training + validation > 99) {
+      throw new Error("Training, testing and validation percents must summary equals 100.");
+    }
+    this.configuration.training = training;
+    this.configuration.validation = validation;
+    this.configuration.testing = 100 - training - validation;
+  }
+
+  public feedWardPropagation() {
+
+  }
+
+  public backWardPropagation() {
+
+  }
+
+  private sigmoid(x: number): number {
+    return 1 / (1 - Math.exp(-x));
+  }
+
+  private dSigmoid(x: number) {
+    let fx = this.sigmoid(x);
+    return fx * (1 - fx);
+  }
+
+  private mseLoss(yTrue: number, yPred: number) {
+    return Math.pow(yTrue - yPred, 2) / 2;
+  }
+
+  private distribute(samples: number[]) {
+    if (samples && samples.length > 0) {
+      let length = samples.length;
+      let trainingLength = this.getNewLength(length, this.configuration.training);
+      let validationLength = this.getNewLength(length, this.configuration.validation);
+      this._trainingSamples = samples.slice(0, trainingLength);
+      this._validationSamples = samples.slice(trainingLength, trainingLength + validationLength);
+      this._testingSamples = samples.slice(trainingLength + validationLength, length);
+
+      console.log(this._trainingSamples);
+      console.log(this._validationSamples);
+      console.log(this._testingSamples);
+    }
+  }
+
+  private getNewLength(length: number = 0, percent: number = 0) {
+    if (length !== 0 && percent !== 0) {
+      return Math.floor((percent / 100) * length);
+    }
+  }
+
+  private datasetToVector() {
+    this.loadDataset().subscribe((dataset) => {
+      this._currentDataset = dataset;
+
+      let lines = dataset.split("\n")
+        .map(line => line.trim())
+        .filter(line => line !== "");
+      let classes = new Set();
+      let resultVector = [];
+      for (const lineStr of lines) {
+        let line = lineStr.split(" ").map(value => value.trim());
+        line = line.slice(1, line.length);
+        let clazz = line[line.length - 1];
+        classes.add(clazz);
+        let lineVector = [
+          ...line.slice(0, line.length - 1).map(element => Number.parseFloat(element)),
+          this.indexOf(clazz, classes)
+        ];
+        resultVector.push(lineVector);
+      }
+      this.configuration.outputCount = 1;
+      this.configuration.inputCount = resultVector[0].length - this.configuration.outputCount;
+      this.configuration.hiddenCount = Math.round((this.configuration.inputCount + this.configuration.outputCount) / 2);
+      this.configuration.contextCount = this.configuration.hiddenCount;
+      this.distribute(resultVector);
+    }, (error) => {
+      console.error(error);
+    })
+  }
+
+  private indexOf(value: any, set: Set<any>): number {
+    let i = 0;
+    for (const element of set) {
+      if (element === value) {
+        return i;
+      }
+      i++;
+    }
+    return -1;
+  }
+
+  private loadDataset() {
+    return this.http.get("../../assets/irisDataset.txt", {
+      responseType: "text"
+    });
   }
 
 }
